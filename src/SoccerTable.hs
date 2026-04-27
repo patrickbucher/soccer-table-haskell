0a6e37ef58ec21925473c602000b3a35ea156afa
Patrick Bucher <patrick.bucher@mailbox.org>
2026-04-27 07:24:44 +0200

    WIP: parse game result

--- src/SoccerTable.hs
+++ src/SoccerTable.hs
@@ -1,4 +1,25 @@
-module SoccerTable (greet) where
+module SoccerTable (GameResult, parse) where
+import Text.Regex.Base
+import Text.Regex.Posix ((=~~))
 
-greet :: String -> String
-greet whom = "Hello, " <> whom <> "!"
+data GameResult = GameResult
+  { homeTeam :: String
+  , awayTeam :: String
+  , homeGoals :: Int
+  , awayGoals :: Int
+  }
+  deriving (Show)
+
+parse :: String -> Maybe GameResult
+parse result =
+  let
+    pattern = "^(.+) ([0-9]+):([0-9]+) (.+)$"
+  in
+    case result =~~ pattern of
+      Just (_, _, _, [homeTeam, homeGoals, awayGoals, awayTeam]) -> Just GameResult
+        { homeTeam = homeTeam
+        , awayTeam = awayTeam
+        , homeGoals = read homeGoals :: Int
+        , awayGoals = read awayGoals :: Int
+        }
+      _ -> Nothing
