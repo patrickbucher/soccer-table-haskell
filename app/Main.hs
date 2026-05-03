de1be7289a2510226898f3c808d48e742537ae90
Patrick Bucher <patrick.bucher@mailbox.org>
2026-05-03 15:05:21 +0200

    read files, calculate table

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,27 @@
 module Main where
-import qualified System.Environment as SE (getArgs)
+import qualified Data.List.Split as S (splitOn)
+import qualified SoccerTable as ST (calculateTable)
+import qualified System.Environment as Env (getArgs)
+import qualified System.Exit as Ex (exitWith, ExitCode(..))
+import qualified System.IO as IO (IOMode(..), hGetContents, openFile)
+import qualified System.Directory.Extra as Dir (listFiles)
+
+slurp :: FilePath -> IO [String]
+slurp f = do
+  h <- IO.openFile f IO.ReadMode
+  c <- IO.hGetContents h
+  let l = S.splitOn "\n" c
+  return l
+
 
 main :: IO ()
-main =
-  do
-    args <- SE.getArgs
-    let
-      dir = 
-        if null args
-        then error "usage: soccer-table DIR"
-        else (head args)
-    putStrLn dir
+main = do
+  args <- Env.getArgs
+  path <-
+    if null args
+    then Ex.exitWith (Ex.ExitFailure 1)
+    else return (head args)
+  files <- Dir.listFiles path
+  results <- mapM slurp files
+  let table = ST.calculateTable $ concat results
+  print table
