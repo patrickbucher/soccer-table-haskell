c87a1c6d339f8bd34869ce76110c65fefc816443
Patrick Bucher <patrick.bucher@mailbox.org>
2026-05-05 16:47:23 +0200

    simplified IO code

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,13 @@ import qualified Formatting as F (formatTable)
 import qualified SoccerTable as ST (calculateTable)
 import qualified System.Environment as Env (getArgs)
 import qualified System.Exit as Ex (exitWith, ExitCode(..))
-import qualified System.IO as IO (IOMode(..), hGetContents, openFile)
+import qualified System.IO as IO (readFile)
 import qualified System.Directory.Extra as Dir (listFiles)
 
 slurp :: FilePath -> IO [String]
 slurp f = do
-  h <- IO.openFile f IO.ReadMode
-  c <- IO.hGetContents h
-  let l = S.splitOn "\n" c
-  return l
+  c <- IO.readFile f
+  return $ S.splitOn "\n" c
 
 main :: IO ()
 main = do
