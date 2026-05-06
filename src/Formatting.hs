67f9874318eced7376ae676e2486048c95f8451d
Patrick Bucher <patrick.bucher@mailbox.org>
2026-05-06 08:41:23 +0200

    rework of formatting (avoid redundant col specs)

--- src/Formatting.hs
+++ src/Formatting.hs
@@ -2,26 +2,10 @@ module Formatting (formatTable)
 where
 import qualified SoccerTable as ST (TableEntry(..))
 
-padLeft :: String -> Int -> String
-padLeft s n = take (n - length s) (repeat ' ') <> s
-
-padRight :: String -> Int -> String
-padRight s n = s <> take (n - length s) (repeat ' ')
-
 data Align = L | R
 
-formatRow :: [(String, Align, Int)] -> String
-formatRow cols = interpose (map align cols) " "
-  where
-    align (s, L, n) = padRight s n
-    align (s, R, n) = padLeft s n
-
-interpose :: [String] -> String -> String
-interpose ss s = concat $ map (\(l, r) -> l <> r) $ zip ss (repeat s)
-
-titleRow :: String
-titleRow =
-  formatRow
+colSpec :: [(String, Align, Int)]
+colSpec =
   [ ("#", R, 3)
   , ("Team", L, 32)
   , ("P", R, 2)
@@ -33,22 +17,49 @@ titleRow =
   , ("=", R, 3)
   ]
 
+formatRow :: [(String, Align, Int)] -> String
+formatRow cols = interpose (map align cols) " "
+  where
+    align (s, L, n) = padRight s n
+    align (s, R, n) = padLeft s n
+
+padLeft :: String -> Int -> String
+padLeft s n = take (n - length s) (repeat ' ') <> s
+
+padRight :: String -> Int -> String
+padRight s n = s <> take (n - length s) (repeat ' ')
+
+interpose :: [String] -> String -> String
+interpose ss s = concat $ map (\(l, r) -> l <> r) $ zip ss (repeat s)
+
+titleRow :: String
+titleRow = formatRow colSpec
+
 separatorRow :: String
-separatorRow = take 58 (repeat '-')
+separatorRow =
+  let
+    c = sum $ map (\(_, _, n) -> n) colSpec
+    s = (length colSpec) - 1
+  in
+    take (c + s) $ repeat '-'
 
 formatTableEntry :: ST.TableEntry -> String
 formatTableEntry e =
-  formatRow
-  [ ((show $ ST.rank e), R, 3)
-  , ((ST.name e), L, 32)
-  , ((show $ ST.points e), R, 2)
-  , ((show $ ST.won e), R, 2)
-  , ((show $ ST.tied e), R, 2)
-  , ((show $ ST.lost e), R, 2)
-  , ((show $ ST.scored e), R, 2)
-  , ((show $ ST.conceded e), R, 2)
-  , ((show $ ST.difference e), R, 3)
-  ]
+  let
+    fields =
+      [ show $ ST.rank e
+      , ST.name e
+      , show $ ST.points e
+      , show $ ST.won e
+      , show $ ST.tied e
+      , show $ ST.lost e
+      , show $ ST.scored e
+      , show $ ST.conceded e
+      , show $ ST.difference e
+      ]
+    cols = zipWith (\f (_, a, w) -> (f, a, w)) fields colSpec
+  in
+    formatRow cols
 
 formatTable :: [ST.TableEntry] -> String
 formatTable t = 
